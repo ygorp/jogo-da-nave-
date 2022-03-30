45e20fbf62471414b47e14337a3fc5cb06185014
ygorp <ygor-nogueira@hotmail.com>
2022-03-29 21:22:59 -0300

    Função de tiro

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function start() {
 
   //Principais variáveis do jogo
 
+  var podeAtirar = true
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334)
@@ -72,6 +73,7 @@ function start() {
 
     if (jogo.pressionou[TECLA.D]) {
       //Chama função Disparo
+      disparo()
     }
   } // fim da função movejogador()
 
@@ -104,4 +106,32 @@ function start() {
       $('#amigo').css('left', 0)
     }
   } // fim da função moveamigo()
+
+  function disparo() {
+    if (podeAtirar == true) {
+      podeAtirar = false
+
+      topo = parseInt($('#jogador').css('top'))
+      posicaoX = parseInt($('#jogador').css('left'))
+      tiroX = posicaoX + 190
+      topoTiro = topo + 37
+      $('#fundoGame').append("<div id='disparo'></div")
+      $('#disparo').css('top', topoTiro)
+      $('#disparo').css('left', tiroX)
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30)
+    } //Fecha podeAtirar
+
+    function executaDisparo() {
+      posicaoX = parseInt($('#disparo').css('left'))
+      $('#disparo').css('left', posicaoX + 15)
+
+      if (posicaoX > 900) {
+        window.clearInterval(tempoDisparo)
+        tempoDisparo = null
+        $('#disparo').remove()
+        podeAtirar = true
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
 } // Fim da função start
